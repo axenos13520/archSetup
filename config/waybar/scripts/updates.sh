d0420a08ac9fd6dc2d09d50b753b28750cf8743e
Adil Nurzhanov <adil.nurzhanov@gdpartners.asia>
2025-12-03 12:10:18 +0500

    update waybar updates.sh

--- config/waybar/scripts/updates.sh
+++ config/waybar/scripts/updates.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-updates=$(checkupdates 2>/dev/null | wc -l)
+while true; do
+    updates_output=$(checkupdates)
+
+    if [[ $? -eq 0 ]]; then
+        updates=$(echo "$updates_output" | wc -l)
+        break
+    fi
+
+    sleep 1
+done
 
 if [ "$updates" -eq 0 ]; then
     text="󰸞"
