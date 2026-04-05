7e17f7ac299f9af3458887d5445b271b6fa3c723
Adil Nurzhanov <adil.nurzhanov@gdpartners.asia>
2026-04-06 02:04:49 +0500

    update cpp launch script

--- features/launch.sh
+++ features/launch.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-{ time cmake -B build >/dev/null && cmake --build build >/dev/null; } |& grep real | sed -E 's/[^0-9\.]+//g' | tr -d '\n' |
+build_time=$({ time { cmake -B build -DCMAKE_COLOR_DIAGNOSTICS=ON >/dev/null && cmake --build build >build_log 2>&1; }; } 2>&1)
+status=$?
+
+if [ $status -ne 0 ]; then
+    echo "-- (!!) Build failed"
+    cat build_log
+    rm build_log
+    exit $status
+fi
+
+rm build_log
+
+echo "$build_time" | grep real | sed -E 's/[^0-9\.]+//g' | tr -d '\n' |
     (cat && echo " * 1000") | bc | xargs printf "-- Build completed in %.2f ms\n\n"
 
 { time ./build/app >/dev/tty; } |& grep real | sed -E 's/[^0-9\.]+//g' | tr -d '\n' |
